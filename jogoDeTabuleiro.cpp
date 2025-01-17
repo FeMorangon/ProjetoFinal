636a488499bb6fc0e31a257488ee82ee4ff9fbab
analara4 <analarapachecoj@gmail.com>
2025-01-17 13:44:46 -0300

    Atualizando jogoDeTabuleiro.cpp
    Signed-off-by: analara4 <analarapachecoj@gmail.com>

--- jogoDeTabuleiro.cpp
+++ jogoDeTabuleiro.cpp
@@ -1,1 +1,13 @@
+#include "jogoDeTabuleiro.hpp"
 
+JogoDeTabuleiro::JogoDeTabuleiro(int linhas, int colunas) : linhas(linhas), colunas(colunas) {
+    tabuleiro.resize(linhas, std::vector<char>(colunas, ' '));
+} // Cada elemento é inicializado com o caractere de espaço, para garantir que, no início do jogo, todas as posições do tabuleiro estejam vazias
+
+char JogoDeTabuleiro::getPosicao(int linha, int coluna) const {
+    return tabuleiro[linha][coluna];
+} // Acessar os valores do tabuleiro, uma vez que os atributos são privados
+
+int JogoDeTabuleiro::getColunas() const {
+    return colunas;
+}
