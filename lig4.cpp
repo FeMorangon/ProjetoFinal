6f3ffbfd3f9ebc146e04be95232723b7d3d76900
analara4 <analarapachecoj@gmail.com>
2025-01-17 14:00:48 -0300

    Atualizando lig4.cpp
    Método encontrarLinhaLivre -> utilizando um loop for, a partir de uma jogada, busca a primeira linha disponível na coluna escolhida e retorna -1 se a coluna estiver cheia;
    
    Método fazerJogada -> utilizando uma estrutura condicional para verificação, a jogada é completa se a linha está disponível (ou seja, se a variável linha for diferente de -1)
    
    Signed-off-by: analara4 <analarapachecoj@gmail.com>

--- lig4.cpp
+++ lig4.cpp
@@ -1,1 +1,26 @@
+#include <iostream>
+#include "lig4.hpp"
 
+Lig4::Lig4(int linhas, int colunas) : JogoDeTabuleiro(6,7) {}
+
+int Lig4::encontrarLinhaLivre(int coluna) {
+    if (coluna < 0 || coluna > colunas) {
+        std::cerr << "A coluna eh invalida." << std::endl;
+    }
+
+    for (int i = linhas - 1; i >= 0; --i) {
+        if (tabuleiro[i][coluna] == ' ') {
+            return i;
+        } //  Determina a primeira linha disponível na coluna escolhida -> de 5 a 0
+    }
+    return -1; // Coluna cheia
+}
+
+bool Lig4::fazerJogada(int coluna, char jogador) {
+    int linha = encontrarLinhaLivre(coluna);
+    if (linha != -1) {
+        tabuleiro[linha][coluna] = jogador;
+        return true; // Jogada válida
+    }
+    return false; // Jogada inválida
+}
