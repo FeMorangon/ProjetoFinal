c23578f56da1d4b5f36b053e7d77ca2d626bf817
analara4 <analarapachecoj@gmail.com>
2025-01-17 13:32:28 -0300

    Atualizando jogoDeTabuleiro.hpp
    Métodos específicos para o jogo Lig4
    
    Signed-off-by: analara4 <analarapachecoj@gmail.com>

--- jogoDeTabuleiro.hpp
+++ jogoDeTabuleiro.hpp
@@ -1,5 +1,9 @@
+// Declaração da classe base
+
 #ifndef JOGODETABULEIRO_H
 #define JOGODETABULEIRO_H
+
+#include <vector>
 
 class JogoDeTabuleiro {
 protected:
@@ -14,6 +18,16 @@ public:
     virtual bool fazerJogada(int coluna, char jogador) = 0;
     char getPosicao(int linha, int coluna) const;
     int getColunas() const;
+
+// para o Lig4
+    virtual bool verificarVertical(char jogador) = 0;
+    virtual bool verificarHorizontal(char jogador) = 0;
+    virtual bool verificarDiagonal(char jogador) = 0;
+    
+    virtual bool verificarEmpate() = 0; // Verificar se todas as posições do tabuleiro estão preenchidas
+    virtual bool verificarVitoria(char jogador) = 0; // No lig4, implementar verificação de vitória na horizontal, vertical e diagonal
+
+    virtual void imprimirTabuleiro() = 0;
 };
 
 #endif
