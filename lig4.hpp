e05ff35f3e7fb0472574235616e9dfebcc8cb3b7
analara4 <analarapachecoj@gmail.com>
2025-01-17 13:50:03 -0300

    Atualizando lig4.hpp
    Criando a declaração da classe Lig4, que deriva da classe base (JogoDeTabuleiro) 
    
    Signed-off-by: analara4 <analarapachecoj@gmail.com>

--- lig4.hpp
+++ lig4.hpp
@@ -1,1 +1,14 @@
+#ifndef LIG4_H
+#define LIG4_H
 
+#include "jogoDeTabuleiro.hpp"
+
+class Lig4 : public JogoDeTabuleiro {
+public:
+    // Construtor
+    Lig4(int linhas, int colunas);
+    int encontrarLinhaLivre(int coluna);
+    bool fazerJogada(int coluna, char jogador) override;
+};
+
+#endif
