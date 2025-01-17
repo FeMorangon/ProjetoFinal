9dc7977815195e0e9349fa9f6cb70f0093d58374
analara4 <analarapachecoj@gmail.com>
2025-01-17 13:29:45 -0300

    Atualizando jogoDeTabuleiro.hpp
    Atributos protegidos podem ser acessados por funções de membro da classe que os declarou, classes derivadas com acesso público ou protegido da classe que os declarou e amigos da classe que os declarou.
    
    Os métodos e atributos serão revisados e atualizados para abranger todos os três jogo.
    
    Signed-off-by: analara4 <analarapachecoj@gmail.com>

--- jogoDeTabuleiro.hpp
+++ jogoDeTabuleiro.hpp
@@ -2,7 +2,18 @@
 #define JOGODETABULEIRO_H
 
 class JogoDeTabuleiro {
+protected:
+    int linhas;
+    int colunas;
+    std::vector<std::vector<char>> tabuleiro;
 
+public:
+    JogoDeTabuleiro(int linhas, int colunas);
+    virtual ~JogoDeTabuleiro() = default;
+
+    virtual bool fazerJogada(int coluna, char jogador) = 0;
+    char getPosicao(int linha, int coluna) const;
+    int getColunas() const;
 };
 
 #endif
