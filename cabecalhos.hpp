b7133ea8aa0cd3418ee25a16079770f97ae98d2e
FeMorangon <fefemorangon@gmail.com>
2025-01-21 20:05:51 -0300

    Update and rename cadastro.cpp to cabecalhos.hpp
    Signed-off-by: FeMorangon <fefemorangon@gmail.com>

---
+++ cabecalhos.hpp
@@ -0,0 +1,26 @@
+#ifndef _CABECALHOS_HPP_
+#define _CABECALHOS_HPP_
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <windows.h>
+#include <random>
+#include <fstream>
+#include <algorithm>
+#include <sstream>
+#include <cmath>
+#include <ctime>
+using namespace std;
+
+const int velha = 3;
+const int reversi = 4;
+const int connect4 = 5;
+const int sair = 1;
+const int voltar = 2;
+const int contra_pessoa = 1;
+const int contra_computador = 2;
+
+string opcao_invalida = "ERRO: op��o inv�lida. Escolha novamente: ";
+
+#endif
